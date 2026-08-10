69a92f901bdfb842b52fa0f248c4a309ec027b9f
祁步鹤 <3227538674@qq.com>
2026-08-10 18:09:11 +0800

    frontend basics

---
+++ src/main/java/org/example/ai_mha/AiMhaApplication.java
@@ -0,0 +1,13 @@
+package org.example.ai_mha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ai_mha/AiMhaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ai_mha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
